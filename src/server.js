7a2e5ead7772a0ba1408d88b27575f45add7652a
Marcin Tichoniuk <marcin@sology.eu>
2025-10-14 15:36:30 +0200

    feat: add get and post endpoints

--- src/server.js
+++ src/server.js
@@ -15,6 +15,32 @@ app.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
 });
 
+app.get('/todos', (req, res) => {
+  return res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+  const todoId = parseInt(req.params.id, 10);
+
+  const todo = todos.find((todo) => todo.id === todoId);
+
+  if (!todo) return res.status(404).json({ message: 'Todo not found' });
+
+  return res.json(todo);
+});
+
+app.post('/todos', (req, res) => {
+  const { text } = req.body;
+
+  if (!text) return res.status(400).json({ message: 'Text is required' });
+
+  const newTodo = { id: todos.length + 1, text, completed: false };
+
+  todos.push(newTodo);
+
+  return res.json(newTodo);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
