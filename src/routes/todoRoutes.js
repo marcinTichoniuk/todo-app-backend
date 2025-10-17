74eab5de28ff027b14551b95af4a493a9cb915e7
Marcin Tichoniuk <marcin@sology.eu>
2025-10-17 11:09:25 +0200

    feat: add validate middleware to routes

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { createTodo, deleteTodo, getAllTodos, getTodoById, updateTodo } from '../controllers/todoController.js';
+import { validate } from '../middlewares/validate.js';
+import { createTodoSchema, todoIdSchema, updateTodoSchema } from '../validators/todoSchema.js';
 
 const router = Router();
 
 router.get('/', getAllTodos);
-router.get('/:id', getTodoById);
-router.post('/', createTodo);
-router.put('/:id', updateTodo);
-router.delete('/:id', deleteTodo);
+router.get('/:id', validate(todoIdSchema, 'params'), getTodoById);
+router.post('/', validate(createTodoSchema, 'body'), createTodo);
+router.put('/:id', validate(todoIdSchema, 'params'), validate(updateTodoSchema, 'body'), updateTodo);
+router.delete('/:id', validate(todoIdSchema, 'params'), deleteTodo);
 
 export default router;
