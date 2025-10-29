cc2819f5c9a600331a3e46cb345abc33de566457
Marcin Tichoniuk <marcin@sology.eu>
2025-10-29 17:52:16 +0100

    feat: start server and connect DB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express, { json } from 'express';
 import todoRoutes from './routes/todoRoutes.js';
 import { config } from './config/config.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const PORT = config.port;
@@ -10,13 +11,30 @@ app.use(json());
 
 // endpoints
 app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+  return res.json({
+    message: 'TODO API is running',
+    version: config.apiVersion,
+    environment: config.environment,
+    database: 'connected',
+  });
 });
 
 // routes
 app.use('/todos', todoRoutes);
 
 // start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
-});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${config.port}...`);
+    });
+  } catch (error) {
+    console.log('Failed to start the server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
